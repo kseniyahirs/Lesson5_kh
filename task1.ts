e457541e2f9c888502c5098063009c6418295d7c
kseniyahirs <kseniya.hirs@gmail.com>
2026-09-08 20:31:17 +0300

    feat: kh_resolved_task1

--- task1.ts
+++ task1.ts
@@ -1,1 +1,10 @@
 // Написать функцию, которая будет высчитывать сумму чисел от нуля, до параметра, который мы в неё передаем.
+console.log(sumNumbers(3));
+
+function sumNumbers(num: number) {
+  let result: number = 0;
+  for (let i = 0; i <= num; i++) {
+    result += i;
+  }
+  return result;
+}
