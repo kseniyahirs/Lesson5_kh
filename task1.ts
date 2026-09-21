005f5b508db2d60c5963809bfc9c42fe4d29a8e1
kseniyahirs <kseniya.hirs@gmail.com>
2026-09-21 20:30:23 +0300

    resolved_task5_kh

--- task1.ts
+++ task1.ts
@@ -1,10 +1,10 @@
 // Написать функцию, которая будет высчитывать сумму чисел от нуля, до параметра, который мы в неё передаем.
-console.log(sumNumbers(3));
+console.log(sumNumbers(9));
 
 function sumNumbers(num: number) {
   let result: number = 0;
   for (let i = 0; i <= num; i++) {
-    result += i;
+    result = result + i;
   }
   return result;
 }
--- task5.ts
+++ task5.ts
@@ -2,16 +2,23 @@
 // Саму задачу обернуть в отдельную функцию getDate, которая принимает в качестве параметра произвольную дату в формате '2026-10-22T22:10:15'
 //* Проверить валидна ли дата в переданном параметре
 
-const now: Date = new Date();
+const addZero = (num: number): string => num.toString().padStart(2, "0");
 
-const day = now.getDate().toString().padStart(2, "0");
-const month = (now.getMonth() + 1).toString().padStart(2, "0");
-const year = now.getFullYear();
+const getDate = (dateString: string): void => {
+  const date = new Date(dateString);
 
-const hours = now.getHours().toString().padStart(2, "0");
-const minutes = now.getMinutes().toString().padStart(2, "0");
-const seconds = now.getSeconds().toString().padStart(2, "0");
+  if (isNaN(date.getTime())) {
+    console.log("Ошибка: Передана некорректная дата!");
+    return;
+  }
+  const day = addZero(date.getDate());
+  const month = addZero(date.getMonth() + 1); // +1, так как месяцы в JS начинаются с 0
+  const year = date.getFullYear();
+  const hours = addZero(date.getHours());
+  const minutes = addZero(date.getMinutes());
+  const seconds = addZero(date.getSeconds());
+  const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  console.log(formattedDate);
+};
 
-const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
-
-console.log(formattedDate);
+getDate("2026-10-22T22:10:15");
